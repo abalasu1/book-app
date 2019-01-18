bab8c94fbed6719ec35021a0f39773b91a49f090
abalasu1 <abalasu1@in.ibm.com>
2019-01-18 22:16:30 +0530

    add security to config service

--- bk-rating-service/src/main/java/com/demo/bookapp/BkRatingServiceApplication.java
+++ bk-rating-service/src/main/java/com/demo/bookapp/BkRatingServiceApplication.java
@@ -3,14 +3,33 @@ package com.demo.bookapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.netflix.hystrix.contrib.javanica.aop.aspectj.HystrixCommandAspect;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.context.annotation.AdviceMode;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
+@EnableTransactionManagement(order=Ordered.LOWEST_PRECEDENCE, mode=AdviceMode.ASPECTJ)
 public class BkRatingServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BkRatingServiceApplication.class, args);
 	}
+	
+    @Bean
+    @Primary
+    @Order(value=Ordered.HIGHEST_PRECEDENCE)
+    public HystrixCommandAspect hystrixAspect() {
+      return new HystrixCommandAspect();
+    }
 
 }
 
